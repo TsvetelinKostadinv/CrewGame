908ae242d1ba3d14e722e37f3fc5643a5531a7aa
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-08-16 16:45:18 +0300

    Reorganised the Constants enum. It is now more convenient for work

--- src/com/crewgame/java/general/Constants.java
+++ src/com/crewgame/java/general/Constants.java
@@ -1,56 +1,52 @@
 package com.crewgame.java.general;
 
-public enum Constants {
-	
-	MISSING_ELEMENT_PLACEHOLDER ( "#" ),
-	
-	/*
-	 * Paths to files & row of paths
-	 */
-	PATH_TO_ABSTRACT_PATHS_FILE ("resources/AbstractPaths.txt"),
-	ROW_OF_BUILDINGS_PATH ( 1 ),
-	ROW_OF_TERRAIN_PATH ( 2 );
-//	public static final int rowOfPathSpeedsPath = 3;
-//	public static final int rowOfCoeficientsForTypeOfRoadPath= 4;
-//	public static final int rowOfAchievementNamesPath = 5;
-//	public static final int rowOfWorldSaveFile = 6;
-	
-	private String str = null;
-	private Integer num = null;
-	
-	Constants(String str)
+public enum Constants 
+{
+	; // this is here because Java requires the values in the enum to be the very first thing you define
+	public static enum Paths
 	{
-		this.str = str;
-	}
-	
-	Constants(int num)
-	{
-		this.num = num;
-	}
-	
-	
-	public Object getValue()
-	{
-		if(str == null)
-		{
-			return num;
-		}else {
-			return str; 
-		}
+		PATH_TO_ABSTRACT_PATHS_FILE ("resources/AbstractPaths.txt");
 		
+		private String value = null;
+		
+		private Paths(String value) { this.value = value; }
+		
+		public String getValue() { return value; }
 	}
-//	/*
-//	 * Path Constants
-//	 */
-//	public static final int indexOfRoadSpeed = 0;
-//	public static final int indexOfRailRoadSpeed = 1;
+	
+	public static enum RowsOfPaths
+	{
+		ROW_OF_BUILDINGS_PATH ( 1 );
+		
+		private Integer value = null;
+		
+		private RowsOfPaths(Integer value) { this.value = value; }
+		
+		public Integer getValue() { return value; }
+	}
+	
+	public static enum CrewGameSpecialSymbols
+	{
+		MISSING_ELEMENT_PLACEHOLDER ( "#" ),
+		SAVES_DATA_SEPARATOR_SYMBOL (",");
+		
+		private String value = null;
+		
+		private CrewGameSpecialSymbols(String value) { this.value = value; }
+		
+		public String getValue() { return value; }
+	}
+//	MISSING_ELEMENT_PLACEHOLDER ( "#" ),
 //	
-	
 //	/*
-//	 * Path coeficients
+//	 * Paths to files & row of paths
 //	 */
-//	public static final int indexOfRoadSpeedCoeficient = 0;
-//	public static final int indexOfRailRoadSpeedCoeficient = 1;
-	
-	
+//	PATH_TO_ABSTRACT_PATHS_FILE ("resources/AbstractPaths.txt"),
+//	ROW_OF_BUILDINGS_PATH ( 1 ),
+//	ROW_OF_TERRAIN_PATH ( 2 );
+////	public static final int rowOfPathSpeedsPath = 3;
+////	public static final int rowOfCoeficientsForTypeOfRoadPath= 4;
+////	public static final int rowOfAchievementNamesPath = 5;
+////	public static final int rowOfWorldSaveFile = 6;
+
 }
