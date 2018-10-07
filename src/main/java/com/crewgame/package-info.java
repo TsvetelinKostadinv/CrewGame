7fa1069e3485a64c659465b5cf96888bf7f7e41f
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-10-07 14:37:09 +0300

    Test For signing
    Signed-off-by: Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>

---
+++ src/main/java/com/crewgame/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * 30/08/2018 at 16:38:53
+ * package-info.java created by Tsvetelin
+ */
+/**
+ * 
+ * The base package for game logic related code
+ * 
+ * @author Tsvetelin
+ *
+ */
+package com.crewgame;
