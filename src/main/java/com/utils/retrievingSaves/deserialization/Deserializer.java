bf0ecf0141a45488f835e70aa35d842050753e62
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2019-05-05 13:52:01 +0300

    Improved the deserializer in order to remove some possibilities of errors

--- src/main/java/com/utils/retrievingSaves/deserialization/Deserializer.java
+++ src/main/java/com/utils/retrievingSaves/deserialization/Deserializer.java
@@ -1,17 +1,19 @@
 
-
 /*
  * 05/09/2018 at 23:06:58
  * Deserializer.java created by Tsvetelin
  */
 package com.utils.retrievingSaves.deserialization;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Optional;
 
 import com.utils.retrievingSaves.ReaderFromSaves;
+
 
 /**
  * 
@@ -22,19 +24,24 @@ import com.utils.retrievingSaves.ReaderFromSaves;
  */
 public class Deserializer implements ReaderFromSaves
 {
-    private ObjectInputStream in;
 
-    /* (non-Javadoc)
-     * @see com.utilsTest.retrievingSaves.ReaderFromSaves#readFromSave(java.io.File, java.lang.Class)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.utilsTest.retrievingSaves.ReaderFromSaves#readFromSave(java.io.File,
+     * java.lang.Class)
      */
-    @SuppressWarnings ( "unchecked" )
     @Override
-    public < T > T readFromSave ( File location , Class< T > desiredObject )
+    public < T > Optional< T > readFromSave (
+            File location ,
+            Class< T > desiredObject )
     {
         Object readObj = null;
         try
         {
-            in = new ObjectInputStream( new FileInputStream( location ) );
+            ObjectInputStream in = new ObjectInputStream(
+                    new FileInputStream( location ) );
             readObj = in.readObject();
             in.close();
         } catch ( IOException e )
@@ -44,10 +51,14 @@ public class Deserializer implements ReaderFromSaves
         {
             e.printStackTrace();
         }
-        
-        return (T) readObj; //TODO FIX THIS SHIT
-        //return TypeConverter.convert( readObj , desiredObject );
+
+        try
+        {
+            return Optional.of( desiredObject.cast( readObj ) );
+        } catch ( ClassCastException e )
+        {
+            return Optional.empty();
+        }
     }
-    
-   
+
 }
--- src/main/java/com/utils/saving/serialization/Serializer.java
+++ src/main/java/com/utils/saving/serialization/Serializer.java
@@ -1,10 +1,10 @@
 
-
 /*
  * 05/09/2018 at 22:42:42
  * Serializer.java created by Tsvetelin
  */
 package com.utils.saving.serialization;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,36 +14,40 @@ import java.io.Serializable;
 
 import com.utils.saving.Saver;
 
+
 /**
  * 
- * Simple implementation of the <code>Saver</code> interface employing the <code>Serializable</code>
+ * Simple implementation of the <code>Saver</code> interface employing the
+ * <code>Serializable</code>
  * 
  * @author Tsvetelin
  *
  */
 public class Serializer implements Saver
 {
-    private ObjectOutputStream out;
 
-    /* (non-Javadoc)
-     * @see com.utilsTest.saving.Saver#save(java.io.File, com.crewgame.base.BaseGameObject)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.utilsTest.saving.Saver#save(java.io.File,
+     * com.crewgame.base.BaseGameObject)
      */
     @Override
     public < T extends Serializable > void save ( File location , T save )
     {
-        if(location != null)
+        if ( location != null )
         {
             try
             {
-                out = new ObjectOutputStream( new FileOutputStream(location) );
+                ObjectOutputStream out = new ObjectOutputStream(
+                        new FileOutputStream( location ) );
                 out.writeObject( save );
                 out.close();
             } catch ( IOException e )
             {
                 e.printStackTrace();
-            }finally {
-                out = null;
             }
+
         }
     }
 }
