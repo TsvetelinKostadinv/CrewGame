3dbd23b936bf6b9b3b36b5702a67f50964ef55b7
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2019-05-27 11:54:02 +0300

    Removed redundant code

--- src/main/java/com/crewgame/world/util/generators/localMapGenerator/LocalMapGenerator.java
+++ src/main/java/com/crewgame/world/util/generators/localMapGenerator/LocalMapGenerator.java
@@ -17,33 +17,31 @@ import com.crewgame.world.util.generators.worldTile.WorldTileGenerator;
 public interface LocalMapGenerator
 {
 
-    public static LocalMap generate ( final WorldTileGenerator generator , final int xSize , final int ySize )
+    public static LocalMap generate (
+            final WorldTileGenerator generator ,
+            final int xSize ,
+            final int ySize )
     {
         System.out.println( "Generating..." );
         LocalMap local = LocalMap.create( xSize , ySize );
-        
-        
-        do{
-            local
-                    .addOnNextAvailableCorrdinate(
-                            generator.generateTile() );
-        }while ( !local.isFull() );
-
-        return local;
-        
-    }
-
-    public static LocalMap mock ()
-    {
-        LocalMap local = LocalMap.EMPTY_DEFAULT_LOCAL_MAP;
 
         while ( !local.isFull() )
         {
             local
                     .addOnNextAvailableCorrdinate(
-                            WorldTileGenerator.mock() );
+                            generator.generateTile() );
         }
 
         return local;
+
     }
+
+    public static LocalMap mock ()
+    {
+        return generate(
+                WorldTileGenerator::mock ,
+                LocalMap.DEFAULT_X_RANGE ,
+                LocalMap.DEFAULT_Y_RANGE );
+    }
+    
 }
